d6e30f982ab3c434c19c359d9728ad042a44b5a8
lc6464 <64722907+lc6464@users.noreply.github.com>
2023-01-15 12:36:44 +0800

    重新提交在学校的一些更改（GPG Key 问题）

--- Indexer.cs
+++ Indexer.cs
@@ -1,27 +1,31 @@
-﻿namespace 其他人文件夹索引工具 {
-	/// <summary>
-	/// 储存此应用程序相关信息的静态类。
-	/// </summary>
-	internal static class OthersIndexer {
-		public const string ApplicationVersion = "v0.1.0.0";
-		public const string ApplicationName = "其他人文件夹索引工具";
-		public const string ApplicationUpdateDate = "2022-03-25";
-		public const string ApplicationFullName = $"{ ApplicationName } { ApplicationVersion } { ApplicationUpdateDate }";
-	}
+﻿using System.Reflection;
 
-	/// <summary>
-	/// 一个索引项。
-	/// </summary>
-	public class OneOtherIndex {
-		[JsonPropertyName("GUID")]
-		public Guid Guid { get; set; }
-		public string? Name { get; set; }
-	}
+namespace 其他人文件夹索引工具;
 
-	/// <summary>
-	/// 索引项列表。
-	/// </summary>
-	public class OthersIndexes {
-		public OneOtherIndex[]? Index { get; set; }
-	}
+/// <summary>
+/// 储存此应用程序相关信息的静态类。
+/// </summary>
+internal static class OthersIndexer {
+	public static readonly Assembly ApplicationAssembly = typeof(OthersIndexer).Assembly;
+	public static readonly string ApplicationVersion = ApplicationAssembly.GetName().Version!.ToString();
+	public static readonly string ApplicationCopyright = ApplicationAssembly.GetCustomAttribute<AssemblyCopyrightAttribute>()!.Copyright;
+	public const string ApplicationName = "其他人文件夹索引工具";
+	public static readonly DateTime ApplicationUpdateDate = new(2023, 1, 6);
+	public static readonly string ApplicationFullName = $"{ ApplicationName } { ApplicationVersion } { ApplicationUpdateDate }";
+}
+
+/// <summary>
+/// 一个索引项。
+/// </summary>
+public class OneOtherIndex {
+	[JsonPropertyName("GUID")]
+	public Guid Guid { get; set; }
+	public string? Name { get; set; }
+}
+
+/// <summary>
+/// 索引项列表。
+/// </summary>
+public class OthersIndexes {
+	public OneOtherIndex[]? Index { get; set; }
 }
